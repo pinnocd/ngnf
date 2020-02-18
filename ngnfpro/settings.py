e0fedfa9c102a4ff833650267e9098d1446da845
pinnocd <dean_pinnock@yahoo.com>
2020-02-18 23:25:18 +0000

    Added Heroku Changes

--- ngnfpro/settings.py
+++ ngnfpro/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
+from decouple import config
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -126,3 +129,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+django_heroku.settings(locals())
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
